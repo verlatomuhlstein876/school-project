7c3c2a2f541b752eb20c15ecd29e0518a355e38f
verlatomuhlstein876 <verlatomuhlstein876@gmail.com>
2025-03-18 01:42:29 +0000

    Add 13.js

---
+++ src/13.js
@@ -0,0 +1,9 @@
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomNumber = getRandomNumber(0, 100);
+console.log(randomNumber);
