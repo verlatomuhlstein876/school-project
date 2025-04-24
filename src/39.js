51ba048b62c6ffde261a629745cac58b16617c93
verlatomuhlstein876 <verlatomuhlstein876@gmail.com>
2025-04-24 15:11:52 +0000

    Add 39.js

---
+++ src/39.js
@@ -0,0 +1,9 @@
+// Student name: Xiaowang
+// Class: Grade 8
+
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    console.log(`The sum of ${i} is: ${sum}`);
+    sum += i;
+}
+console.log(`The total sum is: ${sum}`);
